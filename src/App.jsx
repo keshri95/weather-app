c211c9bb00de4b2b4eb48799c4899c016dd454d3
Ashish Keshri <keshri.ashish1@gmail.com>
2023-01-06 15:02:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,32 @@ function App() {
   const [state, setState] = useState(null);
   const [cityName, setCityName] = useState("Patna");
   // const [loading, setLoading] = useState(true);
-
   const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=a696b790e34d6285a4e972a9b9eb9390`;
 
 
+  const getWeatherData = async (url) => {
+    try {
+      const data = await axios.get(url);
+      const res = await data.data;
+      console.log(res);
+
+      setState(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getWeatherData(API);
+    }, 1000);
+  }, [cityName]);
+
+  const cityNameChangeHandler = (e) => {
+    console.log(e.target.value);
+
+      setCityName(e.target.value);
+  };
 
   return (
     <div className="container">
@@ -26,15 +48,22 @@ function App() {
             />
           </div>
 
-          <div>
-            feels_like : 12.45
-             humidity : 82 
-             pressure : 1022 
-             temp : 12.96
-            temp_max : 12.96 temp_min : 12.96
-          </div>
+          <div className="row">
+            {!state ? (
+              <p>No data found!</p>
+            ) : (
+              <div>
+                <p className="fs-3">{cityName}</p>
+                <p>Huidity: {state.main.humidity} </p>
+                <p>Temp {state.main.temp} C </p>
 
-          
+                <p>
+                  <span>Max {state.main.temp_max} C</span>{" "}
+                  <span>Min {state.main.temp_min} C </span>
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
